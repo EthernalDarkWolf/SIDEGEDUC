455719dcd3a9aa12016551143cc2c4ffbca1cc39
Technolobito <technolobito@gmail.com>
2025-10-24 11:36:38 -0400

    Cambios y uso de flax (tiene error)

--- app.py
+++ app.py
@@ -1,8 +1,27 @@
 from flask import Flask, render_template, request
+from users_and_rols.users import usuarios
+
 app = Flask(__name__)
 
 
-app.route('/')
+#cargar el esquema de entrada (login):
+@app.route('/')
 def index():
-    return render_template(index.html)
-    
+    return render_template(login.html)
+
+@app.route('/login', methods=['GET', 'POST'])# ruta para evitar el error 404 en el login
+
+
+
+#procesar y renderizar el otro esquema
+@app.route('/login', methods=['POST'])
+def login():
+    usario = request.form['user']
+    clave = request.form['pw']
+    if usuarios.get(usuario) == clave:
+        return render_template('panel.html', usuario=usuario)
+    else:
+        print("Error Usuario o clave incorrectos")
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ users_and_rols/users.py
@@ -0,0 +1,6 @@
+usuarios = {
+    "admin": 1234,
+    "Anthony Parada": 1234,
+    "Javier": 1234,
+    "Carmen": 1234
+}
